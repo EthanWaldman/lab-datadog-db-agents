a1f6af3eb83f3a13dd78c61644183c0feccc481d
Ethan Waldman <ewaldman@ust.balance-it-services.com>
2025-09-30 16:20:47 -0600

    Add functionality to dynamically discovery and inventory RDS and Aurora databases for monitoring in place of autodiscovery that currently does not work; including tag-based selection

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Define the Datadog configuration templates to be inserted into the agent containers at deployment time
 locals {
   dd_autodiscovery_postgres_config_yaml = <<EOI
 ad_identifiers:
@@ -48,6 +49,7 @@ EOI
   entry_point_command = var.autodiscovery_enabled ? "echo $CONFIGFILE_POSTGRES | base64 -d > /etc/datadog-agent/conf.d/postgres.d/conf.yaml;echo $CONFIGFILE_DATADOG | base64 -d > /etc/datadog-agent/datadog.yaml; entrypoint.sh" : "echo $CONFIGFILE_POSTGRES | base64 -d > /etc/datadog-agent/conf.d/postgres.d/conf.yaml; entrypoint.sh"
 }
 
+# Retrieve Datadog API Key from AWS Secrets Manager
 data "aws_secretsmanager_secret" "dd_api_key" {
   name = "lab_dd_api_key"
 }
@@ -55,6 +57,7 @@ data "aws_secretsmanager_secret_version" "dd_api_key" {
   secret_id = data.aws_secretsmanager_secret.dd_api_key.id
 }
 
+# Retrieve Postgresql password for datadog user from AWS Secrets Manager
 data "aws_secretsmanager_secret" "dd_db_password" {
   name = "lab_dd_db_password"
 }
@@ -62,6 +65,54 @@ data "aws_secretsmanager_secret_version" "dd_db_password" {
   secret_id = data.aws_secretsmanager_secret.dd_db_password.id
 }
 
+# KNOWN ISSUE: The autodiscovery functionality for Datadog agents does not work when running on Fargate
+#              due to lack of access to the AWS IMDS API; Datadog documentation indicates this problem
+#              and there are suggested fixes that have not proven to work so this is still under
+#              investigation
+# Work-around: Use Terraform to scan the AWS RDS inventory and provision static agents in order
+#              to avoid manually configuring every database (i.e., "poor man's autodiscovery")
+data "aws_rds_clusters" "rds_cluster_list" {
+  region = var.aws_region
+}
+data "aws_rds_cluster" "rds_cluster_list" {
+  for_each = toset(data.aws_rds_clusters.rds_cluster_list.cluster_identifiers)
+
+  cluster_identifier = each.value
+}
+data "aws_db_instances" "rds_instance_list" {
+  region = var.aws_region
+}
+data "aws_db_instance" "rds_instance_list" {
+  for_each = toset(data.aws_db_instances.rds_instance_list.instance_identifiers)
+
+  db_instance_identifier = each.value
+}
+# Terraform inventory discovery will filter on the basis of a tag set to true (default is
+# datadoghq.com/scrape to match real autodiscovery default behavior); when the monitoring_tag_flag
+# is set to an empty string, no filtering will take place and all discovered databases will be monitored
+locals {
+  rds_aurora_cluster_list = var.monitoring_tag_flag == "" ? {
+    for dbkey,dbdetails in data.aws_rds_cluster.rds_cluster_list:
+    dbkey => ({"endpoint" = dbdetails.endpoint, "port" = dbdetails.port, "engine" = dbdetails.engine})
+  } : {
+    for dbkey,dbdetails in data.aws_rds_cluster.rds_cluster_list:
+    dbkey => ({"endpoint" = dbdetails.endpoint, "port" = dbdetails.port, "engine" = dbdetails.engine}) if try(dbdetails.tags[var.monitoring_tag_flag],"") == "true"
+  }
+  rds_db_instance_list = var.monitoring_tag_flag == "" ? {
+    for dbkey,dbdetails in data.aws_db_instance.rds_instance_list:
+    dbkey => ({"endpoint" = split(":",dbdetails.endpoint)[0], "port" = dbdetails.port, "engine" = dbdetails.engine}) if startswith(dbdetails.engine,"postgres")
+  } : {
+    for dbkey,dbdetails in data.aws_db_instance.rds_instance_list:
+    dbkey => ({"endpoint" = split(":",dbdetails.endpoint)[0], "port" = dbdetails.port, "engine" = dbdetails.engine}) if startswith(dbdetails.engine,"postgres") && try(dbdetails.tags[var.monitoring_tag_flag],"") == "true"
+  }
+  postgres_db_list = merge(local.rds_aurora_cluster_list,local.rds_db_instance_list)
+}
+output "postgres_db_list" {
+  value = local.postgres_db_list
+}
+
+# Default behavio will be to use the same VPC where RDS databases are located, but if the
+# dedicated_vpc_flag is set, a separate VPC for monitoring will be provisioned instead
 resource "aws_vpc" "monitoring" {
   count = var.dedicated_vpc_flag ? 1 : 0
 
@@ -72,11 +123,12 @@ resource "aws_vpc" "monitoring" {
     name = "Monitoring VPC"
   }
 }
-
 locals {
   monitoring_vpc_id = var.dedicated_vpc_flag ? aws_vpc.monitoring[0].id : var.rds_vpc_id
 }
 
+# A separate public subnet for ECS will be provisioned for the Datadog agents since they need
+# outbound access to the Internet to reach Datadog APIs but we want to keep RDS isolated
 resource "aws_subnet" "ecs_public" {
   vpc_id            = local.monitoring_vpc_id
   cidr_block        = var.ecs_cidr_block
@@ -87,11 +139,12 @@ resource "aws_subnet" "ecs_public" {
   }
 }
 
-# Create an ECS Cluster
+# Provision the ECS Cluster on Fargate to run the Datadog agents
 resource "aws_ecs_cluster" "dd_fargate_cluster" {
   name = "dd-fargate-cluster"
 }
 
+# Because the ECS subnet will need Internet access, provision an Internet Gateway
 resource "aws_internet_gateway" "monitoring_igw" {
   count = var.dedicated_vpc_flag ? 1 : 0
 
@@ -107,6 +160,7 @@ locals {
   monitoring_igw = var.dedicated_vpc_flag ? aws_internet_gateway.monitoring_igw[0].id : data.aws_internet_gateway.rds_igw.id
 }
 
+# Provision the default route to the Internet for the ECS public subnet
 resource "aws_route_table" "monitoring" {
   vpc_id = local.monitoring_vpc_id
   route {
@@ -130,7 +184,7 @@ resource "aws_security_group" "ecs_service_sg" {
   }
 }
 
-# Create an IAM Role for ECS Task Execution
+# Create a clean IAM Role for ECS Task Execution
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "dd-ecs-task-execution-role"
 
@@ -148,11 +202,12 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
-# Attach Task Execution policy to the execution role
+# Attach to the clean IAM role standard ECS task execution permissions
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+# Additionally attach the permissions required for auto-discovery (which currently is not used)
 resource "aws_iam_role_policy_attachment" "rds_readonly_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess"
@@ -164,7 +219,7 @@ resource "aws_iam_role_policy_attachment" "rds_readonly_role_policy" {
 #   policy_arn = "arn:aws:iam::aws:policy/aws-service-role/AmazonECSServiceRolePolicy"
 # }
 
-# Create Service policy to the execution role
+# Create Service policy to also attach to the IAM role (intent is narrower permissions than default policy)
 resource "aws_iam_role_policy" "ecs_service_policy" {
   name = "dd-ecs-service-policy"
   role = aws_iam_role.ecs_task_execution_role.id
@@ -194,7 +249,8 @@ resource "aws_iam_role_policy" "ecs_service_policy" {
   })
 }
 
-# Create policy for ECS Exec in ECS containers
+# Create an additional policy for ECS Exec in ECS containers - this is needed only for troubleshooting
+# Note: This may not be compliant with corporate security standards so it probably should be removed
 resource "aws_iam_role_policy" "ecs_exec_policy" {
   name = "dd-ecs-exec-policy"
   role = aws_iam_role.ecs_task_execution_role.id
@@ -215,12 +271,14 @@ resource "aws_iam_role_policy" "ecs_exec_policy" {
   })
 }
 
-# Create a CloudWatch Log Group for container logs
+# Create a CloudWatch Log Group for Datadog agent container logs
 resource "aws_cloudwatch_log_group" "dd_app_logs" {
   name              = "/ecs/dd-ecs-app"
   retention_in_days = 3
 }
 
+# If autodiscovery is enabled (currently does not work) then create a single task definition to discover
+# all database targets in the current AWS account and in the assigned region
 resource "aws_ecs_task_definition" "postgres_dd_agent_autodiscovery" {
   count = var.autodiscovery_enabled ? 1 : 0
 
@@ -267,8 +325,10 @@ resource "aws_ecs_task_definition" "postgres_dd_agent_autodiscovery" {
     }
   ])
 }
+# When NOT using autodiscovery (only option currently working) then use the previously discovered
+# list of RDS and Aurora databases to configure one task definition per database monitoring target
 resource "aws_ecs_task_definition" "postgres_dd_agent_static" {
-  for_each                 = var.autodiscovery_enabled ? {} : var.postgres_db_list
+  for_each                 = var.autodiscovery_enabled ? {} : local.postgres_db_list
 
   family                   = "dd-agent-postgres-task"
   cpu                      = var.task_specs["agent"].vcpu
@@ -301,16 +361,17 @@ resource "aws_ecs_task_definition" "postgres_dd_agent_static" {
       environment = [
         { name = "DD_DD_URL", value = var.dd_api_endpoint },
         { name = "CONFIGFILE_POSTGRES", value = base64encode(local.postgres_config_yaml) },
-        { name = "DD_RDS_ENDPOINT", value = "${each.value.rds_endpoint}" },
-        { name = "DD_RDS_PORT", value = "${each.value.rds_port}" },
+        { name = "DD_RDS_ENDPOINT", value = "${each.value.endpoint}" },
+        { name = "DD_RDS_PORT", value = "${tostring(each.value.port)}" },
         { name = "DD_RDS_DB_USERNAME", value = var.dd_db_username },
-        { name = "DD_RDS_REGION", value = "${each.value.rds_region}" },
+        { name = "DD_RDS_REGION", value = var.aws_region },
       ]
     }
   ])
 }
 
-# Define an ECS Service
+# If autodiscovery is enabled (currently does not work) then create a single ECS service to discover
+# all database targets in the current AWS account and in the assigned region
 resource "aws_ecs_service" "postgres_dd_agent_autodiscovery" {
   count = var.autodiscovery_enabled ? 1 : 0
 
@@ -335,9 +396,10 @@ resource "aws_ecs_service" "postgres_dd_agent_autodiscovery" {
     assign_public_ip = true
   }
 }
-# Define an ECS Service
+# When NOT using autodiscovery (only option currently working) then use the previously discovered
+# list of RDS and Aurora databases to provisoin ECS service per database monitoring target
 resource "aws_ecs_service" "postgres_dd_agent_static" {
-  for_each        = var.autodiscovery_enabled ? {} : var.postgres_db_list
+  for_each        = var.autodiscovery_enabled ? {} : local.postgres_db_list
 
   name            = "dd-agent-${each.key}"
   cluster         = aws_ecs_cluster.dd_fargate_cluster.id
--- variables.tf
+++ variables.tf
@@ -1,23 +1,43 @@
+# Terraform provisioning is constrained to a single AWS account and region
 variable "aws_region" {
   type = string
   default = "us-east-1"
 }
-variable "autodiscovery_enabled" {
-  type = bool
-  default = false
-}
-variable "dedicated_vpc_flag" {
-  type = bool
-  default = false
-}
+
+# Manual assignment of the RDS VPC where the databases to monitor are located and where, by default,
+# the ECS cluster and services will be provisioned
 variable "rds_vpc_id" {
   type = string
   default = "vpc-0b54f6bbcb3b174f2"
 }
+
+# Manual assignment of CIDR block for the public ECS subnet where agents will run
 variable "ecs_cidr_block" {
   type = string
   default = "10.0.4.0/24"
 }
+
+# When set to true, provision for Datadog autodiscovery (which currently does NOT work)
+variable "autodiscovery_enabled" {
+  type = bool
+  default = false
+}
+
+# When set to true, provision a separate VPC to contain the ECS subnet and keep outbound Internet
+# traffic off of the RDS VPC (e.g., as a method for tracking via Flow Logs)
+variable "dedicated_vpc_flag" {
+  type = bool
+  default = false
+}
+
+# When string is populated, use as a tag filter to only select databases for monitoring if named tag
+# is set to true (default value matches the behavior of Datadog autodiscovery)
+variable "monitoring_tag_flag" {
+  type = string
+  default = "datadoghq.com/scrape"
+}
+
+# Specifications for ECS tasks - defaults probably would not require any change
 variable "task_specs" {
   type = map(object({
     count = number
@@ -32,6 +52,8 @@ variable "task_specs" {
     }
   }
 }
+
+# Datadog agent container image details, including version to be kept up-to-date over time
 variable "container_image" {
   type = map(object({
     image_name = string
@@ -45,27 +67,15 @@ variable "container_image" {
   }
 }
 
+# Username for agent authentication to Postgres databases (password to be kept in Secrets Manager)
 variable "dd_db_username" {
   type = string
   default = "datadog"
 }
+
+# API endpoint for Datadog
 variable "dd_api_endpoint" {
   type = string
   default = "https://us3.datadoghq.com"
-}
-
-variable "postgres_db_list" {
-  type = map(object({
-    rds_endpoint = string
-    rds_port = string
-    rds_region = string
-  }))
-  default = {
-    lab = {
-      rds_endpoint = "labdb.clgyiws0up99.us-east-1.rds.amazonaws.com"
-      rds_port = "5432"
-      rds_region = "us-east-1"
-    }
-  }
 }
 
